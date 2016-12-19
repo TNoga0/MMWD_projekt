44af986f56644b85bb4cd7d1a01b5858e619ea52
pachnacyrozmaryn <michalpiekars@gmail.com>
2016-12-19 17:44:58 +0100

    Update taboo.cpp

--- taboo.cpp
+++ taboo.cpp
@@ -101,14 +101,36 @@ void taboo::resizeWynik(int n)
     n_wyniku=n;
 }
 
-bool taboo::czyNiePuste()
+void taboo::resizeZaladowanie(int n)
+{
+    int **nowa = new int * [n];
+    for (int i = 0; i<3; i++)
+    {
+
+        nowa[i] = new int [n];
+    }
+    for(i=0;i<3;i++)
+    {
+        for(int j=0;j<n-1;j++)
+            nowa[i][j]=zaladowanie_samolotu[i][j];
+            nowa[i][n]=zaladowanie_samolotu[i][n-1];
+    }
+    for (i = 0; i<3; i++)
+        delete [] zaladowanie_samolotu[i];
+    delete [] zaladowanie_samolotu;
+    zaladowanie_samolotu=nowa;
+
+}
+									 
+									 
+bool taboo::czyNiePuste(int **tab)
 {
     int k=0;
     for(int i=0,i<rozmiar;i++)
         {
         for(int j=0;j<rozmiar;j++)
             {
-                if(lista_przewozowa[i][j]!=0)
+                if(tab[i][j]!=0)
                     k=1;
             }
         }
@@ -117,108 +139,83 @@ bool taboo::czyNiePuste()
     if(k==0)
         return false;
 }
+									 
+int taboo::maximum(int *tab)
+{
+    int tmp=0;
+    int wiersz;
+    for(int i=0;i<rozmiar;i++)
+    {
+        if(tab[i]>tmp)
+        {
+             tmp=tab[i];
+             wiersz=i;
+        }
+
+    }
+    return wiersz;
+}
+
 
 void taboo::InitialSolution()
 {
     int n_wyniku=1;
+    int ktoraiteracja=0;
     int **tmp_wynik = new int * [n_wyniku];
     for (int i = 0; i<3; i++)
     {
 
-        nowa[i] = new int [n];
+        nowa[i] = new int [n_wyniku];
     }
-    int **temp1;
-    int **temp2;
-    int **temp3;
+    int *temp1;
+    int *temp2;
+    int *temp3;
 
-    temp1 = new int * [rozmiar];
-    temp2 = new int * [rozmiar];
+    temp1 = new int[rozmiar];
+    temp2 = new int[rozmiar];
     temp3 = new int[rozmiar];
-    for (int i = 0; i<rozmiar; i++)
-    {
-        temp1[i] = new int [rozmiar];
-        temp2[i] = new int [rozmiar];
-        temp3[i] = new int [rozmiar];
-    }
 
 	for(int i =0; i<rozmiar; i++)
     {
-        for (int j =0; j<rozmiar; j++)
-        {
-            temp1[i][j]=0;
-            temp2[i][j]=0;
-            temp3[i][j]=0;
+            temp1[i]=0;
+            temp2[i]=0;
+            temp3[i]=0;
 
-        }
     }
     int counter=0;
-    int wziete=0;
-    int wziete1=0;
+    int odkad1=0;
+    int odkad2=0;
+    int odkad3=0;
     //duza petla
-    while(czyNiePuste())
+    while(czyNiePuste(lista_przewozowa))
     {
-        while((wziete1<3*ladownosc)&&czyNiePuste())
+        while(((zaladowanie_samolotu[0][ktoraiteracja]+zaladowanie_samolotu[1][ktoraiteracja]+zaladowanie_samolotu[2][ktoraiteracja])<3*ladownosc)&&czyNiePuste())
         {
-            while(wziete<ladownosc)   //1 samolot
+            while((zaladowanie_samolotu[0][ktoraiteracja])<ladownosc)   //1 samolot
             {
-                for(int i =0; i<rozmiar; i++)
+                for(int i =odkad1; i<rozmiar; i++)
                 {
                     for (int j =0; j<rozmiar; j++)
                     {
                        if(lista_przewozowa[i][j]!=0)
                        {
-                           if((wziete+lista_przewozowa[i][j])<=ladownosc)
+                           if(((zaladowanie_samolotu[0][ktoraiteracja])+lista_przewozowa[i][j])<=ladownosc)
                            {
-                               temp1[i][j]=lista_przewozowa[i][j];
-                               wziete=wziete+lista_przewozowa[i][j];
+                               temp1[j]=lista_przewozowa[i][j];
+                               zaladowanie_samolotu[0][ktoraiteracja]=zaladowanie_samolotu[0][ktoraiteracja]+lista_przewozowa[i][j];
                                lista_przewozowa[i][j]=0;
                            }
                            else
                            {
-                               if(wziete==25)
+                               if(zaladowanie_samolotu[0][ktoraiteracja]==25)
                                 {
-                                    //gowno robi
+                                    //nic nie robi
                                 }
                                else
                                 {
-                                 temp1[i][j]=(lista_przewozowa[i][j]-(wziete+lista_przewozowa[i][j]-ladownosc));
-                                 wziete=25;
-                                 lista_przewozowa[i][j]=(wziete+lista_przewozowa[i][j]-ladownosc);
-                                }
-
-                           }
-                       }
-
-                    }
-                }
-            }
-            wziete1=wziete1+wziete;
-            wziete=0;
-            while(wziete<ladownosc)   //2 samolot
-            {
-                for(int i =0; i<rozmiar; i++)
-                {
-                    for (int j =0; j<rozmiar; j++)
-                    {
-                       if(lista_przewozowa[i][j]!=0)
-                       {
-                           if((wziete+lista_przewozowa[i][j])<=ladownosc)
-                           {
-                               temp2[i][j]=lista_przewozowa[i][j];
-                               wziete=wziete+lista_przewozowa[i][j];
-                               lista_przewozowa[i][j]=0;
-                           }
-                           else
-                           {
-                               if(wziete==25)
-                                {
-                                    //gowno robi
-                                }
-                               else
-                                {
-                                 temp2[i][j]=(lista_przewozowa[i][j]-(wziete+lista_przewozowa[i][j]-ladownosc));
-                                 wziete=25;
-                                 lista_przewozowa[i][j]=(wziete+lista_przewozowa[i][j]-ladownosc);
+                                 temp1[j]=(lista_przewozowa[i][j]-(zaladowanie_samolotu[0][ktoraiteracja]+lista_przewozowa[i][j]-ladownosc));
+                                 zaladowanie_samolotu[0][ktoraiteracja]=25;
+                                 lista_przewozowa[i][j]=(zaladowanie_samolotu[0][ktoraiteracja]+lista_przewozowa[i][j]-ladownosc);
                                 }
 
                            }
@@ -228,33 +225,31 @@ void taboo::InitialSolution()
                 }
             }
 
-            wziete1=wziete1+wziete;
-            wziete=0;
-            while(wziete<ladownosc)   //3 samolot
+            while((zaladowanie_samolotu[1][ktoraiteracja])<ladownosc)   //2 samolot
             {
-                for(int i =0; i<rozmiar; i++)
+                for(int i =odkad2; i<rozmiar; i++)
                 {
                     for (int j =0; j<rozmiar; j++)
                     {
                        if(lista_przewozowa[i][j]!=0)
                        {
-                           if((wziete+lista_przewozowa[i][j])<=ladownosc)
+                           if((zaladowanie_samolotu[1][ktoraiteracja]+lista_przewozowa[i][j])<=ladownosc)
                            {
-                               temp3[i][j]=lista_przewozowa[i][j];
-                               wziete=wziete+lista_przewozowa[i][j];
+                               temp2[j]=lista_przewozowa[i][j];
+                               zaladowanie_samolotu[1][ktoraiteracja]=zaladowanie_samolotu[1][ktoraiteracja]+lista_przewozowa[i][j];
                                lista_przewozowa[i][j]=0;
                            }
                            else
                            {
-                               if(wziete==25)
+                               if(zaladowanie_samolotu[1][ktoraiteracja]==25)
                                 {
-                                    //gowno robi
+                                    //nic nie robi
                                 }
                                else
                                 {
-                                 temp3[i][j]=(lista_przewozowa[i][j]-(wziete+lista_przewozowa[i][j]-ladownosc));
-                                 wziete=25;
-                                 lista_przewozowa[i][j]=(wziete+lista_przewozowa[i][j]-ladownosc);
+                                 temp2[j]=(lista_przewozowa[i][j]-(zaladowanie_samolotu[1][ktoraiteracja]+lista_przewozowa[i][j]-ladownosc));
+                                 zaladowanie_samolotu[1][ktoraiteracja]=25;
+                                 lista_przewozowa[i][j]=(zaladowanie_samolotu[1][ktoraiteracja]+lista_przewozowa[i][j]-ladownosc);
                                 }
 
                            }
@@ -263,21 +258,131 @@ void taboo::InitialSolution()
                     }
                 }
             }
-           wziete1=wziete1+wziete;
-           wziete=0;
+
+
+            while((zaladowanie_samolotu[2][ktoraiteracja])<ladownosc)   //3 samolot
+            {
+                for(int i =odkad3; i<rozmiar; i++)
+                {
+                    for (int j =0; j<rozmiar; j++)
+                    {
+                       if(lista_przewozowa[i][j]!=0)
+                       {
+                           if((zaladowanie_samolotu[2][ktoraiteracja]+lista_przewozowa[i][j])<=ladownosc)
+                           {
+                               temp3[j]=lista_przewozowa[i][j];
+                               zaladowanie_samolotu[2][ktoraiteracja]=zaladowanie_samolotu[2][ktoraiteracja]+lista_przewozowa[i][j];
+                               lista_przewozowa[i][j]=0;
+                           }
+                           else
+                           {
+                               if(zaladowanie_samolotu[2][ktoraiteracja]==25)
+                                {
+                                    //nic nie robi
+                                }
+                               else
+                                {
+                                 temp3[j]=(lista_przewozowa[i][j]-(zaladowanie_samolotu[2][ktoraiteracja]+lista_przewozowa[i][j]-ladownosc));
+                                 zaladowanie_samolotu[2][ktoraiteracja]25;
+                                 lista_przewozowa[i][j]=(zaladowanie_samolotu[2][ktoraiteracja]+lista_przewozowa[i][j]-ladownosc);
+                                }
+
+                           }
+                       }
+
+                    }
+                }
+            }
+
 
 
         }
+        ktoraiteracja++;
+//wyladowywanie
 
+//samolot1
+        if(czyNiePuste(temp1))
+        {
+        odkad1=maximum(temp1);
+        tmp_wynik[0][n_wyniku-1]=odkad1;
+        zaladowanie_samolotu[0][n_wyniku-1]=zaladowanie_samolotu[0][n_wyniku-1]-temp1[odkad1];
+        temp1[odkad1]=0;
+        }
+        else{
+            tmp_wynik[0][n_wyniku-1]=0;
+        }
+//samolot2
+        if(czyNiePuste(temp2))
+        {
+        odkad2=maximum(temp2);
+        tmp_wynik[1][n_wyniku-1]=odkad2;
+        zaladowanie_samolotu[1][n_wyniku-1]=zaladowanie_samolotu[1][n_wyniku-1]-temp2[odkad2];
+        temp2[odkad2]=0;
+        }
+        else{
+            tmp_wynik[1][n_wyniku-1]=0;
+        }
+//samolot3
+         if(czyNiePuste(temp3))
+        {
+        odkad3=maximum(temp3);
+        tmp_wynik[2][n_wyniku-1]=odkad3;
+        zaladowanie_samolotu[2][n_wyniku-1]=zaladowanie_samolotu[2][n_wyniku-1]-temp3[odkad3];
+        temp3[odkad3]=0;
+        }
+        else{
+            tmp_wynik[2][n_wyniku-1]=0;
+        }
 
-
+        resizeWynik(n_wyniku+1);
+        resizeZaladowanie(n_wyniku+1);
 
 
 
 
 
     }
-}
 
+    while(czyNiePuste(temp1))
+        {
+        odkad1=maximum(temp1);
+        tmp_wynik[0][n_wyniku-1]=odkad1;
+        zaladowanie_samolotu[0][n_wyniku-1]=zaladowanie_samolotu[0][n_wyniku-1]-temp1[odkad1];
+        temp1[odkad1]=0;
+        }
+    while(czyNiePuste(temp2))
+        {
+        odkad2=maximum(temp2);
+        tmp_wynik[1][n_wyniku-1]=odkad2;
+        zaladowanie_samolotu[1][n_wyniku-1]=zaladowanie_samolotu[1][n_wyniku-1]-temp2[odkad2];
+        temp2[odkad2]=0;
+        }
+    while(czyNiePuste(temp3))
+        {
+        odkad3=maximum(temp3);
+        tmp_wynik[2][n_wyniku-1]=odkad3;
+        zaladowanie_samolotu[2][n_wyniku-1]=zaladowanie_samolotu[2][n_wyniku-1]-temp3[odkad3];
+        temp3[odkad3]=0;
+        }
+
+    best_wynik = new int * [n_wyniku];
+    for (int i = 0; i<3; i++)
+    {
+
+        best_wynik[i] = new int [n_wyniku];
+    }
+    for(i=0;i<3;i++)
+    {
+        for(int j=0;j<n_wyniku;j++)
+            best_wynik[i][j]=tmp_wynik[i][j];
+
+    }
+    for (i = 0; i<3; i++)
+        delete [] tmp_wynik[i];
+    delete [] tmp_wynik;
+
+
+
+}
 
 
