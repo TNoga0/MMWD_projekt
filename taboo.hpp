1d21c8ae2ff992143f9b3eb8eab7b649a33783c9
pachnacyrozmaryn <michalpiekars@gmail.com>
2016-12-19 17:41:49 +0100

    Update taboo.hpp

--- taboo.hpp
+++ taboo.hpp
@@ -13,7 +13,7 @@ private:
     int n_wyniku;
     int **best_wynik;
     int **tmp_wynik;
-    int **zaladowanie_samolotu_alg;
+    int **zaladowanie_samolotu;
     int **ladunek_samolotu; //wiersze ->samoloty, kolumny ->numer lotniska, czyli 3x8
     int **sasiedztwo;
 
@@ -21,15 +21,16 @@ public:
     taboo();
     ~taboo();
     void InitialSolution();
-    bool czyNiePuste();
+    bool czyNiePuste(int **tab);
     void resizeWynik(int n);
-
+    void resizeZaladowanie(int n);
     int ObliczFCelu(int **rozw);  //liczy na podstawie danych z tabel i z wyniku <- dla kazdego rozwiazania, nie wiem czy dobrze wklepane jako arg
     void showZabronienia();
     void showWynik();
     void oblicz_zaladowanie();
     void stworz_sasiedztwo();
     int sprawdz_dopuszczalnosc(int ** rozw);
+    int maximum(int *tab);
     //modyfikacja wyniku
     //uwtorzenie listy zabronien
     //robienie sasiedztwa
